4cbb77ebe63c91acfa04f59f63817ceb44c01d82
taisukim <cookehwk@naver.com>
2023-02-13 17:12:44 +0900

    base code

---
+++ src/main/java/com/example/keesunspringboot/KeesunSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.keesunspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeesunSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeesunSpringBootApplication.class, args);
+    }
+
+}
